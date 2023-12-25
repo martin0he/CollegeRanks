94bd4948bcb9400d221337811023293307536b0f
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-25 14:06:56 +0100

    fixed styling crossover

--- frontend/src/theme.tsx
+++ frontend/src/theme.tsx
@@ -3,5 +3,8 @@ import { createTheme } from '@mui/material';
 export const theme = createTheme({
   typography: {
     fontFamily: [ 'Jost', 'serif' ].join(','),
+    allVariants: {
+      color: '#880C79'
+    }
   },
 });
