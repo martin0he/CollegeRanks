915ebe28a35a4fa147f9a37dd788329bc1374954
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-27 16:00:15 +0100

    improving chart

--- frontend/src/components/rankings-layout/Chart.tsx
+++ frontend/src/components/rankings-layout/Chart.tsx
@@ -5,22 +5,31 @@ import { Box, Typography } from "@mui/material";
 const sdk = new ChartsEmbedSDK({
   baseUrl: "https://charts.mongodb.com/charts-cr-osisi",
   showAttribution: false,
+  
 });
 
 const uniChart: Chart = sdk.createChart({
   chartId: "658c2644-2865-44f5-830f-99b6e885738a",
   autoRefresh: true,
-  background: "inherit",
+  background: "transparent",
   maxDataAge: 120, //refresh rate in seconds
+  renderingSpec: {
+    version: 1,
+    
+
+  }
 });
+
+
 
 const ChartComponent: React.FC = () => {
   useEffect(() => {
     uniChart.render(document.getElementById("chart-data")!);
     // .catch(() => window.alert("Chart failed to initialise"));
+    
   }, []);
 
-  const date = new Date();
+  
 
   return (
     <Box
@@ -30,7 +39,7 @@ const ChartComponent: React.FC = () => {
       <Typography variant="h4" fontWeight="bold">
         University Rankings
       </Typography>
-      <Typography>{`As of: ${date}`}</Typography>
+      
       <div id="chart-data" style={{ height: 500 }}></div>
     </Box>
   );
