835825802504df84dcd88dd1ff465ef27b5c48f2
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-29 02:44:46 +0100

    cleanup

--- backend/src/controllers/unis.ts
+++ backend/src/controllers/unis.ts
@@ -89,52 +89,6 @@ export const updateUni = async (
   }
 };
 
-export const avg = async (req: express.Request, res: express.Response) => {
-  try {
-    const { id } = req.params;
-
-    // Find the university by ID
-    const university = await UniModel.findById(id);
-
-    if (!university) {
-      return res.status(404).json({ error: "University not found" });
-    }
-
-    // Calculate average for each rating subfield
-    const averageRatings: Record<string, number> = {};
-    for (const [metric, values] of Object.entries(university.rating)) {
-      const sum = values.reduce((acc, value) => acc + value, 0);
-      const average = sum / values.length || 0; // Handle division by zero
-      averageRatings[metric] = average;
-    }
-
-    // Calculate weighted average based on weights
-    const weights = {
-      Academics: 0.16,
-      Food: 0.08,
-      Dorms: 0.12,
-      Social: 0.12,
-      Location: 0.15,
-      Opportunities: 0.17,
-      Clubs: 0.08,
-      Safety: 0.12,
-    };
-
-    const weightedAverage = Object.entries(weights).reduce(
-      (acc, [metric, weight]) => acc + weight * averageRatings[metric],
-      0
-    );
-
-    // Round the result to two decimal places
-    const roundedWeightedAverage = Math.round(weightedAverage * 100) / 100;
-
-    return res.status(200).json(roundedWeightedAverage);
-  } catch (error) {
-    console.error("Error calculating weighted average:", error);
-    return res.status(500).json({ error: "Internal Server Error" });
-  }
-};
-
 export const updateUniRating = async (
   req: express.Request,
   res: express.Response
--- backend/src/router/unis.ts
+++ backend/src/router/unis.ts
@@ -1,7 +1,7 @@
 import express from "express";
 
 
-import { getAllUniNames, getUni, getUniID, updateUni, getAllUnis, avg, updateUniRating } from "../controllers/unis";
+import { getAllUniNames, getUni, getUniID, updateUni, getAllUnis, updateUniRating } from "../controllers/unis";
 
 
 
@@ -13,6 +13,5 @@ export default (router: express.Router) => {
     router.get('/unis', getAllUnis); //works
     router.get('/uni/id/:id', getUniID);  //works
     router.patch('/uni/ratings/:id', updateUni);  //works
-    router.post('/uni/rating/:id', avg);  
     router.patch('/uni/rating/:id', updateUniRating);
 };
