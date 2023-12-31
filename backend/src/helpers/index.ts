a3cd099fa5e22cf76aaa8baf0a6f1129baa4306b
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-31 02:07:54 +0100

    gitignore

--- backend/src/helpers/index.ts
+++ backend/src/helpers/index.ts
@@ -1,6 +1,6 @@
 import crypto from "crypto";
 
-const SECRET = "BOOGERS-REST-API";
+const SECRET = process.env.SECRET;
 
 export const random = () => crypto.randomBytes(128).toString("base64");
 export const authentication = (salt: string, password: string) => {
