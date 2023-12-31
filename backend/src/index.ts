012571e488ac9dc7188b568949c898404ff74ef4
Martin Hema <90310958+martin0he@users.noreply.github.com>
2023-12-31 05:17:01 +0100

    fix3

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://collegeranks.onrender.com/"],
+    origin: ["http://localhost:3000", "https://collegeranks.onrender.com"],
     credentials: true,
   })
 );
