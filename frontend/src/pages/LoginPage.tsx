1cf1b93964f9ea079fd141bcdf994b8c07348274
Martin Hema <90310958+martin0he@users.noreply.github.com>
2024-01-06 15:16:58 -0500

    fixed linting

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -48,77 +48,82 @@ export const LoginPage: React.FC = () => {
   };
 
   return (
-    <Box
-      sx={{
-        mt: 5,
-        mb: 3,
-        display: "flex",
-        justifyContent: "center",
-        flexDirection: "column",
-        alignItems: "center",
-      }}
-    >
-      <Avatar sx={{ m: 1, bgcolor: "#e1d6ba" }}>
-        <LoginIcon />
-      </Avatar>
-      <Typography variant="h5" sx={{ color: "inherit" }}>
-        Login
-      </Typography>
-      <Box sx={{ mt: 1 }}>
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="email"
-          label="Email Address"
-          name="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-
-        <TextField
-          margin="normal"
-          required
-          fullWidth
-          id="password"
-          name="password"
-          label="Password"
-          type="password"
-          value={password}
-          onChange={(e) => {
-            setPassword(e.target.value);
-          }}
-        />
-
-        <Button
-          fullWidth
-          variant="contained"
+    <>
+      <Container maxWidth="xs">
+        <CssBaseline />
+        <Box
           sx={{
-            mt: 3,
-            mb: 2,
-            color: "inherit",
-            bgcolor: "inherit",
-            "&:hover": { bgcolor: "#f5f4f3" },
+            mt: 5,
+            mb: 3,
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: "column",
+            alignItems: "center",
           }}
-          onClick={handleLogin}
         >
-          Login
-        </Button>
-        <Grid container justifyContent={"flex-end"}>
-          <Grid item>
-            <AnchorLink
-              style={{
-                textDecoration: "none",
-                color: "black",
-                textDecorationLine: "underline",
+          <Avatar sx={{ m: 1, bgcolor: "#e1d6ba" }}>
+            <LoginIcon />
+          </Avatar>
+          <Typography variant="h5" sx={{ color: "inherit" }}>
+            Login
+          </Typography>
+          <Box sx={{ mt: 1 }}>
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <TextField
+              margin="normal"
+              required
+              fullWidth
+              id="password"
+              name="password"
+              label="Password"
+              type="password"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
               }}
-              href="#signup"
+            />
+
+            <Button
+              fullWidth
+              variant="contained"
+              sx={{
+                mt: 3,
+                mb: 2,
+                color: "inherit",
+                bgcolor: "inherit",
+                "&:hover": { bgcolor: "#f5f4f3" },
+              }}
+              onClick={handleLogin}
             >
-              Don't have an account? Register Here
-            </AnchorLink>
-          </Grid>
-        </Grid>
-      </Box>
-    </Box>
+              Login
+            </Button>
+            <Grid container justifyContent={"flex-end"}>
+              <Grid item>
+                <AnchorLink
+                  style={{
+                    textDecoration: "none",
+                    color: "black",
+                    textDecorationLine: "underline",
+                  }}
+                  href="#signup"
+                >
+                  Don't have an account? Register Here
+                </AnchorLink>
+              </Grid>
+            </Grid>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 };
